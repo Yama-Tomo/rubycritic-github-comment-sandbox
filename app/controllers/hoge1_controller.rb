9e6f29fbf469ba6a33c2d141d694c866805c5df7
Yama-Tomo <mazda13bturbo@gmail.com>
2019-05-10 19:33:08 +0900

    new feature

---
+++ app/controllers/hoge1_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < Devise::SessionsController
+  respond_to :json
+
+  before_action :verify_authentication, only: [:show]
+
+  def show
+    render json: { :user => { id: current_user.id, email: current_user.email } }
+  end
+
+  def hoge
+    b = 1
+  end
+
+  def hoge2
+    a = 1  
+  end
+
+  def hoge3
+    a = 1  
+  end
+
+  def hoge4
+    a = 1  
+  end
+
+  def hoge5
+    a = 1  
+  end
+
+  def verify_authentication
+    unless user_signed_in?
+      render json: { error: 'forbidden' }, status: 403
+      return false
+    end
+  end
+end
